3529c62c866d0e033c984fea98fd42dd8d771ece
chikason <52418760+chikason@users.noreply.github.com>
2019-07-18 14:24:54 +0100

    Update VelaTestApplicationTests.java

--- src/test/java/com/vela/work/VelaTestApplicationTests.java
+++ src/test/java/com/vela/work/VelaTestApplicationTests.java
@@ -6,11 +6,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@DataJpaTest
 @SpringBootTest
 public class VelaTestApplicationTests {
+
+      @Autowired
+      private CardRepository cardRepo;
+
+      @Autowired
+      private StatRepository statRepo;
+
+      @Autowired
+      private TestEntityManager entityManager;
+
 
 	@Test
 	public void contextLoads() {
 	}
+
+        //<summary> checks or tests the working of Card Repository interface </summary>
+        @Test
+        public void testCard(){
+          //set data
+          Card card = new Card(1,519904,"Visa","credit",null);
+          entityManager.persist(card);
+          
+          //find data from table
+          Card card1 = cardRepo.findByCardNo(card.getCardNo());
+
+          //check 
+          asserThat(card1.getScheme())
+                   .isEqualTo(card.getScheme());
+
+   }
 
 }
